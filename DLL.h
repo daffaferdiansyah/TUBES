d9b086a4abdbc3610746c8057984986d520ac8ec
Muhammad Daffa Ferdiansyah <85597705+daffaferdiansyah@users.noreply.github.com>
2021-12-20 11:30:08 +0700

    Update DLL.h

--- DLL.h
+++ DLL.h
@@ -26,17 +26,30 @@ struct Mhs{
 struct elm_MK{
     MK info;
     adr_MK next, prev;
-    adr_jual nextMhs;
+    adr_Mhs nextMhs;
 };
 
 struct elm_Mhs{
     Mhs info;
-    adr_jual next, prev;
+    adr_Mhs next, prev;
 };
 
 struct mll{
-    adr_sales first, last;
+    adr_MK first, last;
 };
 
+void Create_list(mll &List);
+void newElm_MK(MK info, adr_MK &mk);
+void newElm_Mhs(Mhs info, adr_Mhs &mhs);
+void insertNew_Mahasiswa(mll &List, adr_MK mk, adr_Mhs mhs);
+void insertLast_MK(mll &List, adr_MK mk);
+void deleteFirst_Mahasiswa(mll &List, adr_MK mk, adr_Mhs mhs);
+void deleteLast_Mahasiswa(mll &List, adr_MK &mk, adr_Mhs &mhs);
+void deleteAfter_Mahasiswa(mll &List, adr_MK mk, adr_Mhs prec, adr_Mhs &mhs);
+void deleteFirst_Matkul(mll &List, adr_MK mk);
+void deleteLast_Matkul(mll &List, adr_MK mk);
+void deleteAfter_Matkul(mll &List, adr_MK prec, adr_MK &mhs);
+void delete_MK(mll &List, adr_MK mk);
+void delete_Mhs(mll &list, adr_MK mk, adr_Mhs mhs);
 
 #endif // DLL_H_INCLUDED
